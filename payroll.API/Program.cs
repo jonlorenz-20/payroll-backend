56a2d4dce5d20b5c24baf67bbc7604446798cad8
Jon Lorenz Llabres <jonlorenzllabres@gmail.com>
2026-06-03 11:18:38 +0800

    sekyur pass

--- payroll.API/Program.cs
+++ payroll.API/Program.cs
@@ -8,28 +8,56 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        
-        policy.WithOrigins(
-                    "https://reverend-squint-parish.ngrok-free.dev",
-                    "http://localhost",
-                    "https://localhost"
-              )
+        policy.AllowAnyOrigin() 
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
 });
 
-
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
-
-
 builder.Services.AddControllers();
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+app.Use(async (context, next) =>
+{
+    
+    if (context.Request.Path.StartsWithSegments("/swagger"))
+    {
+        
+        if (!context.Request.Headers.ContainsKey("Authorization"))
+        {
+            context.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Secure Payroll Swagger\"";
+            context.Response.StatusCode = 401;
+            return;
+        }
+
+        var authHeader = context.Request.Headers["Authorization"].ToString();
+        if (authHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase))
+        {
+            var credentials = System.Text.Encoding.UTF8.GetString(
+                Convert.FromBase64String(authHeader.Substring(6)))
+                .Split(':');
+
+           
+            // Username: admin | Password: sekyurswaggerpassword!
+            if (credentials[0] == "admin" && credentials[1] == "sekyurswaggerpassword!")
+            {
+                await next();
+                return;
+            }
+        }
+
+        context.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Secure Payroll Swagger\"";
+        context.Response.StatusCode = 401;
+        return;
+    }
+
+    await next();
+});
 
 
 if (app.Environment.IsDevelopment())
@@ -37,17 +65,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
-{
-    
-    app.MapGet("/", () => "Sekyur-Link Payroll API Gateway - Secure Operational Mode Active.");
-}
-
 
 app.UseCors("AllowAll");
 app.UseAuthorization();
-
-
 app.MapControllers();
 
 app.Run();
